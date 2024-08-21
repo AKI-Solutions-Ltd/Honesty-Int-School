6c809c958c6d38e1f847dd2de2e69ce52a7d04b6
Michaelcodify <Michaelcodify0@gmail.com>
2024-08-21 14:27:15 +0100

    form v1

--- contact.php
+++ contact.php
@@ -44,6 +44,28 @@
 	<script src='../../google_analytics_auto.html'></script>
 </head>
 
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    $to = "tobilobamich4ilery@gmail.com";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nPhone: $phone\n\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you! Your message has been sent.";
+    } else {
+        echo "Sorry, something went wrong. Please try again.";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
+
 <body>
 
 	<!-- LOAD PAGE -->
@@ -397,3 +419,6 @@
 </body>
 
 </html>
+
+
+
